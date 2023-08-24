3b78c334b3d2af82ea93f2227ce013b375391ec8
Dayal Fernando <dayalfernando@gmail.com>
2023-08-24 10:37:38 +0530

    initial commit

---
+++ src/main/java/com/local/motocommunityapiportal/MotocommunityApiPortalApplication.java
@@ -0,0 +1,13 @@
+package com.local.motocommunityapiportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotocommunityApiPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotocommunityApiPortalApplication.class, args);
+	}
+
+}
